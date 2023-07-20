77668566a7ff0e15ecda8a4ba171588e6bd374f5
Maggie Reda <magreda16@gmail.com>
2023-07-19 22:43:17 -0400

    modify footer, header styles

--- intsureview_fe/src/Footer/Footer.js
+++ intsureview_fe/src/Footer/Footer.js
@@ -1,11 +1,29 @@
 import * as React from 'react';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 import './Footer.css'
 
 const Footer = () => {
 
   return (
     <div className='footer_container'>
-      <h5 className='footer_title'>2023 Copyright: Scottiestore.com</h5>
+      <ul className='footer_social_icons'>
+        <li><TwitterIcon /></li>
+        <li><FacebookIcon /></li>
+        <li><InstagramIcon /></li>
+        <li><PinterestIcon /></li>
+      </ul>
+      <section className='footer_info'>
+        <ul className='footer_info_btns'>
+          <li>Contact Us</li>
+          <li>About</li>
+          <li>Privacy</li>
+          <li>Careers</li>
+        </ul>
+        <h5 className='footer_title'>© 2023 Copyright | Scottiestore </h5>
+      </section>
     </div>
   );
 }
--- intsureview_fe/src/Header/Header.js
+++ intsureview_fe/src/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PetsOutlinedIcon from '@mui/icons-material/PetsOutlined';
 import './Header.css'
 
 
@@ -7,9 +8,10 @@ const Header = () => {
     <div className="header_container">
       <div className='header_content'>
         <section className='header_title'>
-          <div className='header_logo'></div>
+          <PetsOutlinedIcon sx={{ color: 'white', fontSize: '50px' }} />
           <h1 className='header_company_name'>ScottieStore</h1>
         </section>
+        {/* These "buttons" are just for aesthetic purposes */}
         <ul className='header_nav_btns'>
           <li>Shop</li>
           <li>Supplies</li>
